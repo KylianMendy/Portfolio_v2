910279ed29d12452fb7cf76f018ee8c3efb4852a
kylian <kylian.mendy@hetic.net>
2021-03-26 02:52:00 +0100

    deploy

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,1 +1,39 @@
 
+const projects = document.getElementById('projects');
+const allprojects = document.querySelectorAll('.projects__name li')
+
+let background;
+
+allprojects.map(project =>{
+  project.addEventListener('mouseover',setBackground(project));
+  project.style.backgroundImage = `url(${background})`
+})
+
+const setBackground = (project)=>{
+
+  switch(project){
+    case project.id === 'moode':
+      background = require('../assets/backgroundhome2.png')
+      break;
+    case project.id === 'nike':
+      background = require('../assets/nikebg.png')
+      break;
+    case project.id === 'tnf':
+      break;
+    case project.id === 'sf':
+      break;
+    case project.id === 'primitiv':
+      break;
+    case project.id === 'player':
+      break;
+    case project.id === 'bene':
+      break;
+    case project.id === 'autres':
+      break;
+    default: 
+    background = require('../assets/backgroundhome.png') 
+    break;
+  }
+}
+
+
